32a27f1a2b0e57c491ab09a6e2d88c11cb992e65
André Frélicot <andre@frelicot.com>
2025-12-09 03:59:59 +0100

    Add GitHubLink MDX component for source code links
    New Component:
    - Add GitHubLink component (src/components/mdx/github-link.tsx)
    - Displays GitHub octocat SVG icon with customizable label
    - Styled button with hover effects matching site design
    
    Content Updates:
    - Update LLMVoice article in all locales (en, fr, es, pt)
    - Replace plain markdown link with GitHubLink component
    - Localized labels for each language version

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -1,11 +1,13 @@
 import { AppStoreLink } from "./app-store-link";
 import { Callout } from "./callout";
+import { GitHubLink } from "./github-link";
 import { ThemeImage } from "./theme-image";
 import { ThreeCube } from "./three-cube";
 
 export const mdxComponents = {
   AppStoreLink,
   Callout,
+  GitHubLink,
   ThreeCube,
   ThemeImage,
 };
